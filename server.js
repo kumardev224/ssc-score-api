8efb2ab3cfd4bc110f9cfe6cb73807ff8f85f2e1
kumardev224 <kumardev224@gmail.com>
2026-03-04 16:10:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.get("/score", async (req,res)=>{
     try{
 
         const response = await axios.get(url,{
-            headers:{
-                "User-Agent":"Mozilla/5.0"
-            }
-        });
+    headers:{
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/121 Safari/537.36",
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language":"en-US,en;q=0.9",
+        "Referer":"https://sscexams.cbexams.com/",
+        "Connection":"keep-alive"
+    },
+    timeout:20000
+});
 
         const html = response.data;
 
